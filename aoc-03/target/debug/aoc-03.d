9d6736542716c1f12fbb0c783fd778d7c69e7eef
kanopo <dmitri.ollari@protonmail.com>
2022-12-03 19:27:49 +0100

    Inizio AOC 3

---
+++ aoc-03/target/debug/aoc-03.d
@@ -0,0 +1,1 @@
+/home/user/Documents/AOC2022/aoc-03/target/debug/aoc-03: /home/user/Documents/AOC2022/aoc-03/src/main.rs
---
+++ aoc-03/target/debug/deps/aoc_03-c164ce7ea1b33199.d
@@ -0,0 +1,5 @@
+/home/user/Documents/AOC2022/aoc-03/target/debug/deps/aoc_03-c164ce7ea1b33199: src/main.rs
+
+/home/user/Documents/AOC2022/aoc-03/target/debug/deps/aoc_03-c164ce7ea1b33199.d: src/main.rs
+
+src/main.rs:
