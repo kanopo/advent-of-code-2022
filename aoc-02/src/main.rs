8a9afeb69490fd1c5bf72e6409c92b0c858d72c0
kanopo <dmitri.ollari@protonmail.com>
2022-12-03 12:48:59 +0100

    Inizio AOC-02

---
+++ aoc-02/src/main.rs
@@ -0,0 +1,29 @@
+
+
+
+fn main() {
+
+    let mosse_sasso_carta_forbice_elfi = ["A", "B", "C"];
+    let risposte_sasso_carta_forbici = ["X", "Y", "Z"];
+
+    /*
+        mosse fegli elfi
+
+        A = sasso
+        B = carta
+        C = forbici
+    */
+
+    /*
+        Risposte alle loro mosse
+
+        X = sasso
+        Y = carta
+        Z = forbici
+    */
+
+
+    println!("Hello, world!");
+}
+
+
