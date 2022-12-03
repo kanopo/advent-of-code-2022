2b6bd803a2713d31b07e3617b118e545d933c840
kanopo <dmitri.ollari@protonmail.com>
2022-12-03 12:39:29 +0100

    finito giorno 1

--- aoc-01/src/main.rs
+++ aoc-01/src/main.rs
@@ -5,24 +5,48 @@ use std::io::{self, BufRead};
 
 
 fn main() {
+
+    let mut vec = Vec::new();
+
     if let Ok(lines) = read_lines("./elf_calories.csv") {
+        let mut calories_single_elf: i32 = 0;
         for line in lines {
             if let Ok(line) = line {
-                let res: Result<i32, ParseIntError> = line.trim().parse();
-                // facendo cosi leggo tutte le righe del file
-                // provo a convertirle in int32 e se mi da errore perche 
-                // la riga e' vuota, so che l'elfo e' cambiato
-
-                // TODO: salvare i dati sommati per ogni elfo in un array
+                let res: Result<i32, ParseIntError> = line.trim().parse();  
+                /*
+                Se il parser di int legge correttamente allora c'è un numero,
+                altrimenti c'è uno spazio vuoto e vuol dire che l'elfo è cambiato
+                */
 
                 if let Ok(res) = res {
-                    println!("{}", res)
+                    calories_single_elf = calories_single_elf + res;
                 } else {
-                    println!("cambio")
+
+                    vec.push(calories_single_elf);
+                    calories_single_elf = 0;
                 }
             }
+            
         }
     }
+
+    vec.sort();
+
+    let len = vec.len();
+
+    let max_val = vec.get(len - 1).unwrap();
+
+    let second_max_val = vec.get(len - 2).unwrap();
+
+    let third_max_val = vec.get(len - 3).unwrap();
+
+    println!("Elfo con più calorie: {}", max_val);
+
+    println!("Calorie dei 3 elfi con più calorie: {}", max_val + second_max_val + third_max_val)
+
+    
+
+
 }
 
 
